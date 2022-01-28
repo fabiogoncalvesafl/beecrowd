2a24f72c8a25908d5513fd4b35b8616ded2ccbcf
Fábio Gonçalves <fabiogoncalves22@gmail.com>
2022-01-27 22:49:17 -0300

    Update challenge

---
+++ src/Bee1067.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bee1067 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+
+		for (int i = 1; i <= x; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+
+		sc.close();
+	}
+
+}
